1712f4845cb75c42d8e76f62a0cf032b5d7c9578
Monsmith <Monsmith>
2018-01-22 23:05:06 +0700

    initial

---
+++ lib/mon_smith_view_tool.rb
@@ -0,0 +1,6 @@
+require "mon_smith_view_tool/version"
+require "mon_smith_view_tool/renderer"
+
+module MonSmithViewTool
+  # Your code goes here...
+end
---
+++ lib/mon_smith_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MonSmithViewTool
+  VERSION = "0.1.0"
+end
